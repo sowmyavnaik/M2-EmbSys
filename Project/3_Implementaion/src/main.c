1fa9f04d92166103c68657c56f21295cf3f3dd20
sowmya <vnsowmya6@gmail.com>
2022-03-09 08:08:13 +0530

    update

--- Project/3_Implementaion/src/main.c
+++ Project/3_Implementaion/src/main.c
@@ -2,24 +2,24 @@
 
 void main()
 {
-    PORTA=0X00;           DDRA=0XFF;
-    PORTB=0X00;           DDRB=0X0F;
-    PORTC=0X00;           DDRC=0XFF;
-    PORTD=0X0F;           DDRD=0XF0;
+    PORTA =0X00;           DDRA=0XFF;
+    PORTB =0X00;           DDRB=0X0F;
+    PORTC =0X00;           DDRC=0XFF;
+    PORTD =0X0F;           DDRD=0XF0;
     
-    OCR1AH=0X3D;           OCR1AL=0X09;    //ONE Second equivalent
+    OCR1AH =0X3D;           OCR1AL=0X09;    //ONE Second equivalent
     
-    TIMSK|=0X10;           //Enable compare match interrupt
-    SREG|=0X80;            //Enable global interrupt
+    TIMSK| =0X10;           //Enable compare match interrupt
+    SREG| =0X80;            //Enable global interrupt
     
-    UBRRH=0X00;         UBRRL=0X67;    //Baud rate=9600
-    UCSRB=0X98;         UCSRC=0X86;
+    UBRRH =0X00;         UBRRL=0X67;    //Baud rate=9600
+    UCSRB =0X98;         UCSRC=0X86;
     
-    PORTC|=0X40;      
+    PORTC| =0X40;      
 
     for(temp1=0;temp1<=3;temp1++)
     {
-        password[temp1]=eeprom_rd(temp1);
+        pass[temp1]=eeprom_rd(temp1);
     }
     block=eeprom_rd(10);    //Blocked information
     if(block==1)
